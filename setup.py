3011437a895811fd85c323eaebaa9733bebba9d7
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2022-02-24 22:23:05 +0700

    update setup.py for test-pypi

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
-setup(name='suntime',
-      version='1.2.5',
+setup(name='suntime-yasirroni',
+      version='1.2.5.1',
       description='Simple sunset and sunrise time calculation python library',
       long_description=long_description,
+      long_description_content_type="text/markdown",
       author='Krzysztof Stopa',
-      url='https://github.com/SatAgro/suntime',
+      url='https://github.com/yasirroni/suntime/tree/optimize',
       copyright='Copyright 2019 SatAgro',
       license='LGPLv3',
       packages=['suntime'],
