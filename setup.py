cec4d57b700e7e6f59d098b8c93fd072dc907318
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2022-02-25 09:21:36 +0700

    add __version__

--- setup.py
+++ setup.py
@@ -9,8 +9,16 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
+# get __version__
+import re
+PACKAGE_NAME = 'suntime'
+version_line = open(path.join(this_directory, PACKAGE_NAME, 'version.py'), "rt").read()
+
+m = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_line, re.M)
+__version__ = m.group(1)
+
 setup(name='suntime-yasirroni',
-      version='1.2.5.1',
+      version=__version__,
       description='Simple sunset and sunrise time calculation python library',
       long_description=long_description,
       long_description_content_type="text/markdown",
--- suntime/__init__.py
+++ suntime/__init__.py
@@ -1,1 +1,3 @@
+from .version import __version__
+
 from .suntime import Sun, SunTimeException
